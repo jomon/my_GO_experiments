bd06a108415217205658b4b05d7b1986ba45994e
jomon <jomonsebin@gmail.com>
2015-07-03 11:55:41 +0530

    day 6

--- fileserver/main.go
+++ fileserver/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/russross/blackfriday"
 	"net/http"
+	"os"
 )
+port := os.Getenv("PORT")
+if port == "" {
+port = "8080"
+}
 
 func main() {
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":8080", nil)
+	hhttp.ListenAndServe(":"+port, nil)
 }
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
